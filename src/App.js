be70a5062b5830ac75635b18e0d06e329432af4d
DESKTOP-69B7S7O\Patryk Bokota <patryk.bokota6@gmail.com>
2024-10-01 11:00:42 +0200

    importuje bootstrap do formularza App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,3 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
-}
-
-export default App;
+import React, {useState} from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+import './App.test.css';
