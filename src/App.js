008647e240553c56bf1670fd0579b7aaa86e844a
DESKTOP-69B7S7O\Patryk Bokota <patryk.bokota6@gmail.com>
2024-10-01 11:03:13 +0200

    stworzyłem funkcje App i dodałem stany przechowujące dane formularza

--- src/App.js
+++ src/App.js
@@ -1,3 +1,8 @@
 import React, {useState} from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 import './App.test.css';
+
+function App(){
+  const [tytuł, setTytuł] = useState("");
+  const [rodzaj, setRodzaj] = useState[""];
+}
